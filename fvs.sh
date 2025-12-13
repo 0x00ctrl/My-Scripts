192da46e0a48f0a0096336d91fcd0128b6401181
Abrar <140312789+0x00ctrl@users.noreply.github.com>
2025-12-14 01:18:28 +0500

    Clean up comments in fvs.sh
    Removed usage instructions and comments from fvs.sh.

--- fvs.sh
+++ fvs.sh
@@ -1,21 +1,5 @@
 # wrapper script for preview searching files using fzf
-
-# usage
-# fvs nano    // (You can also use vim, and mousepad or any other text editor(just replace the name in the script)
-# fvs with text editor option will open the view-search window but when you choose a file, it will be 
-# automatically opened in the text editor
-
-# fvs <dir u want to search>
-# if path not specified, present working directory will be searched
-
-# fzf program is a prerequisite for this
-# sudo apt install fzf
-# make sure to give executable permission to this script ->  chmod u+x fvs.sh
-# and place this file inside any directory listed in your PATH environment variable to run it from your present working directory, no need to ./fvs.sh 
-
 #!/bin/bash
-# wrapper script for preview searching files using fzf
-
 if [[ "$1" == "nano" || "$1" == "micro" || "$1" == "vim" || "$1" == "mousepad" ]]; then
  	$1 $(fzf --reverse --preview "cat {}")
     exit 1
